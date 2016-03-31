26cd885f18712947e7f5a3f7d00d99c3150d1248
Preston O'Neal <prestononeal@gmail.com>
2016-03-30 22:17:18 -0700

    Create conference functionality

--- Lesson_4/00_Conference_Central/settings.py
+++ Lesson_4/00_Conference_Central/settings.py
@@ -12,5 +12,5 @@ created/forked from conference.py by wesc on 2014 may 24
 
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
-WEB_CLIENT_ID = 'replace with Web client ID'
+WEB_CLIENT_ID = '626926364898-h53h6sotqe8f3gs9r5h5flj6neq58dhn.apps.googleusercontent.com'
 
